ef7e0b1595de8c65d6afb7d9502dc964412ab527
taniki <tk@deveha.com>
2019-10-11 23:54:47 +0200

    fix build

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,10 @@
-import VueMatomo from 'vue-matomo'
+export default function (Vue, options, { router }) {
+  if (process.isClient) {
+    const { default: VueMatomo } = require('vue-matomo')
 
-export default function (Vue, options, { isServer: disabled, router }) {
-  Vue.use(VueMatomo, {
-    router,
-    disabled,
-    ...options
-  })
+    Vue.use(VueMatomo, {
+      router,
+      ...options
+    })
+  }
 }
